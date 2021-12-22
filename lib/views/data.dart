6f8211c1663e73791e166b57029bfd6ddb45be92
PriscillaFianu <priscillafianu80@gmail.com>
2021-12-22 15:32:25 +0000

    update

---
+++ lib/views/data.dart
@@ -0,0 +1,88 @@
+import 'package:contact_app_2/views/models/details.dart';
+
+class Data {
+  static List<Details> detailsOfPeople() {
+    return [
+      Details(
+          image:
+              "https://images.unsplash.com/photo-1639986587553-1d994ffc66d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
+          name: "Bridget Akorsihu",
+          phone: "0256786453",
+          email: "BridgetAkorsihu@gmail.com",
+          time: "10:00am",
+          region: "Ghana"),
+      Details(
+          image:
+              "https://images.unsplash.com/photo-1640009349927-0ce2dbccca3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
+          name: "mummy love",
+          phone: "0243674052",
+          email: "tempor.diam@aol.edu",
+          time: "10:30am",
+          region: "Ghana"),
+      Details(
+          image:
+              "https://images.unsplash.com/photo-1640039985592-df9c2e3f2605?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
+          name: "Babes",
+          phone: "0554365643",
+          email: "Babes@gmail.com",
+          time: "11:00am",
+          region: "Ghana"),
+      Details(
+          image:
+              "https://images.unsplash.com/photo-1640043285014-3d1e095b3ebe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
+          name: "jenny",
+          phone: "0243567540",
+          email: "eros.proin.ultrices@hotmail.com",
+          time: "12:00pm",
+          region: "Ghana"),
+      Details(
+          image:
+              "https://images.unsplash.com/photo-1593642532454-e138e28a63f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
+          name: "Brian Ramos",
+          phone: "0231234567",
+          email: "at.egestas@aol.couk",
+          time: "1:00pm",
+          region: "Azad Kashmir"),
+      Details(
+          image:
+              "https://images.unsplash.com/photo-1640036279464-732f75d8628b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
+          name: "Carlos Harper",
+          phone: "0243216753",
+          email: "proin.nisl@icloud.org",
+          time: "2:00pm",
+          region: "Guanacaste"),
+      Details(
+          image:
+              "https://images.unsplash.com/photo-1640056844620-0fb7546b3eff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
+          name: "Britanney Dorsey",
+          phone: "0245678651",
+          email: "non@yahoo.couk",
+          time: "3:00pm",
+          region: "Jharkhand"),
+      Details(
+          image:
+              "https://images.unsplash.com/photo-1529704817457-9f19aad72e1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=400&q=60",
+          name: "William Ortiz",
+          phone: "0245674321",
+          email: "molestie.in@yahoo.ca",
+          time: "4:00pm",
+          region: "North Island"),
+      Details(
+          image:
+              "https://images.unsplash.com/photo-1640037984424-ac1a02cb742a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60",
+          name: "Shellie Merrill",
+          phone: "0245632123",
+          email: "non@google.couk",
+          time: "5:00pm",
+          region: "Gorontalo"),
+      Details(
+          image:
+              "https://images.unsplash.com/photo-1640035283313-cc67bd0afc23?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60",
+          name: "Clinton Mosley",
+          phone: "0546732143",
+          email: "sagittis.augue@google.edu",
+          time: "6:00pm",
+          region: "South Chungcheong"),
+    ];
+  }
+}
--- lib/views/models/details.dart
+++ lib/views/models/details.dart
@@ -1,21 +1,17 @@
+class Details {
+  String image = "";
+  String name = "";
+  String phone = "";
+  String email = "";
+  String time = "";
+  String region = "";
 
-
-
-// class Details {
-//    String image="";
-//   String name="";
-//   String phone="";
-//   String email="";
-//   String country="";
-//   String region="";
-
-//   Details({
-//     required this.image,
-//     required this.name,
-//     required this.phone,
-//     required this.email,
-//     required this.country,
-//     required this.region,
-//   })
-
-// }
+  Details({
+    required this.image,
+    required this.name,
+    required this.phone,
+    required this.email,
+    required this.time,
+    required this.region,
+  }); 
+}
